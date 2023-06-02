306d6b6c09f9a282196db00671eded4abc9a6777
JesseKoldewijn <dev@glitchtech.eu>
2023-06-02 16:51:09 +0200

    fix: removed server-action due to current incompatibility with turbopack

--- src/server/dbHelpers/projects.ts
+++ src/server/dbHelpers/projects.ts
@@ -3,6 +3,5 @@ import { db, projects, type tProject } from "../db";
 export const runtime = "edge";
 
 export const getAllProjects = async () => {
-  "use server";
   return (await db.select().from(projects)) as tProject[];
 };
