064ace3a30a4c2a2af494a93f0cbf9063151e05d
JesseKoldewijn <dev@glitchtech.eu>
2023-06-05 23:10:15 +0200

    fix: split of drizzle schemas by file + typo fix

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,7 +1,5 @@
-import { mysqlTable, uniqueIndex, varchar } from "drizzle-orm/mysql-core";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 import { connect } from "@planetscale/database";
-import { type InferModel } from "drizzle-orm";
 
 const connection = connect({
   host: process.env["DATABASE_HOST"],
@@ -10,19 +8,3 @@ const connection = connect({
 });
 
 export const db = drizzle(connection);
-
-export const projects = mysqlTable(
-  "Project",
-  {
-    id: varchar("id", { length: 256 }).primaryKey(),
-    title: varchar("title", { length: 256 }),
-    desc_short: varchar("desc_short", { length: 256 }),
-    desc_long: varchar("desc_long", { length: 256 }),
-  },
-  (projects) => ({
-    nameIndex: uniqueIndex("name_idx").on(projects.title),
-  })
-);
-
-export type tProject = InferModel<typeof projects>; // return type when queried
-export type tInsetProject = InferModel<typeof projects, "insert">; // insert type
---
+++ src/server/schemas/projects.ts
@@ -0,0 +1,18 @@
+import { mysqlTable, uniqueIndex, varchar } from "drizzle-orm/mysql-core";
+import { type InferModel } from "drizzle-orm";
+
+export const projects = mysqlTable(
+  "Project",
+  {
+    id: varchar("id", { length: 256 }).primaryKey(),
+    title: varchar("title", { length: 256 }),
+    desc_short: varchar("desc_short", { length: 256 }),
+    desc_long: varchar("desc_long", { length: 256 }),
+  },
+  (projects) => ({
+    nameIndex: uniqueIndex("name_idx").on(projects.title),
+  })
+);
+
+export type tProject = InferModel<typeof projects>; // return type when queried
+export type tInsertProject = InferModel<typeof projects, "insert">; // insert type
